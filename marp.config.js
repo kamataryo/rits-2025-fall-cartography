53c827dd938c66587fb94c75ca6846cf87a4e3ee
kamataryo <mugil.cephalus+github@gmail.com>
2025-06-01 20:58:09 +0900

    生成スクリプト調整調整

--- marp.config.js
+++ marp.config.js
@@ -9,13 +9,5 @@ module.exports = {
   },
 
   // 数式サポート
-  math: 'katex',
-
-  // ウォッチモードの設定
-  watch: {
-    // ファイル変更時の自動リロード
-    open: true,
-    // ポート番号
-    port: 8080
-  }
+  math: 'katex'
 };
